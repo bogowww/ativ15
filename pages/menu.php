989ab6e088c282e90280d72e47a0cacf9a7dcb3a
Arthur Bogo <arthur.bogo.ifc.riodosul@gmail.com>
2022-11-19 19:41:27 -0300

    Alteração visual.

--- pages/menu.php
+++ pages/menu.php
@@ -7,7 +7,7 @@
       include_once($path);  
 ?>
 
-<nav class="navbar" style="background-color: #e3f2fd;">
+<nav class="navbar" style="background-color: #959595;">
 <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
